233ab7ac6134c52a1910126c30e90a1e8e33f870
maria alejandra cortes duque <cortesmariale@gmail.com>
2023-04-06 12:46:05 -0500

    Iniciando el ruteo y proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
+import Contact from "./Routes/Contact";
+
 
 
 function App() {
   return (
       <div className="App">
-          <Navbar/>
-          <Footer/>
+        <BrowserRouter>
+        <Navbar/>
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/detail/:id" element={<Detail/>}/>
+          <Route path="/favorites/:id" element={<Favs/>}/>
+          <Route path="/contact/:id" element={<Contact/>}/>
+        </Routes>
+        </BrowserRouter>          
+        <Footer/>
       </div>
   );
 }
